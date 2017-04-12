792f8e7f3570af96dfc3eddbe931da99a0e863cf
Dustin Torres <dustingtorres@gmail.com>
2017-04-12 13:57:53 -0700

    Force consent for all from django

--- oauth2client/contrib/django_util/views.py
+++ oauth2client/contrib/django_util/views.py
@@ -68,7 +68,10 @@ def _make_flow(request, scopes, return_url=None):
         scope=scopes,
         state=state,
         redirect_uri=request.build_absolute_uri(
-            urlresolvers.reverse("google_oauth:callback")))
+            urlresolvers.reverse("google_oauth:callback")),
+        prompt="consent",
+        access_type="offline",
+    )
 
     flow_key = _FLOW_KEY.format(csrf_token)
     request.session[flow_key] = jsonpickle.encode(flow)
